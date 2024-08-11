47830ccfcef7309f5f3c11a5c90911c4b284bf02
FT1006 <frankthp1234@gmail.com>
2024-08-12 02:25:44 +0800

    F(1): bug fix

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,6 +19,9 @@ class HTMLNode:
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
 
+    def text_node_to_html_node(text_node):
+        return LeafNode(None, text_node)
+        
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
@@ -29,7 +32,7 @@ class LeafNode(HTMLNode):
         if self.tag is None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
-        
+
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
 
