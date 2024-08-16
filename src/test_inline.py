51e5b861ea8196ba1b837ae8ebe7bdf8c8fc200b
FT1006 <frankthp1234@gmail.com>
2024-08-17 03:51:56 +0800

    K(1): Add unit test for the function: extract_markdown_links

--- src/test_inline.py
+++ src/test_inline.py
@@ -147,6 +147,17 @@ class TestSplitNodesDelimiter(unittest.TestCase):
                 actual = extract_markdown_images(text)
                 self.assertEqual(actual, expected)
 
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://blog.boot.dev"),
+            ],
+            matches,
+        )
 
 
 
