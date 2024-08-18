d8b1d2133353347a6424944779c2912239942a4c
FT1006 <frankthp1234@gmail.com>
2024-08-19 02:04:30 +0800

    N: Add block_to_block_type function and its unit test

--- src/block_md.py
+++ src/block_md.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class Block_Type(Enum):
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+    PARAGRAPH = "paragraph"
+
 def markdown_to_blocks(markdown):
     if not isinstance(markdown, str):
         raise TypeError("Input must be a string")
@@ -9,3 +19,33 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+    if (
+        block.startswith("#") 
+        or block.startswith("##") 
+        or block.startswith("###") 
+        or block.startswith("####") 
+        or block.startswith("#####") 
+        or block.startswith("######")
+    ):
+        return Block_Type.HEADING
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return Block_Type.CODE
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return Block_Type.PARAGRAPH
+        return Block_Type.QUOTE
+    if block.startswith("* ") or block.startswith("- "):
+        for line in lines:
+            if not line.startswith("* ") and not line.startswith("- "):
+                return Block_Type.PARAGRAPH
+        return Block_Type.UNORDERED_LIST
+    if block.startswith("1. "):
+        for i in range(len(lines)):
+            if not lines[i].startswith(f"{i+1}. "):
+                return Block_Type.PARAGRAPH
+        return Block_Type.ORDERED_LIST
+    return Block_Type.PARAGRAPH
--- src/test_block_md.py
+++ src/test_block_md.py
@@ -1,5 +1,6 @@
 import unittest
-from block_md import markdown_to_blocks
+from block_md import *
+from enum import Enum
 
 class TestBlockMD(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -19,6 +20,73 @@ class TestBlockMD(unittest.TestCase):
                         markdown_to_blocks(case["input"])
                 else:
                     actual = markdown_to_blocks(case["input"])
+                    self.assertEqual(actual, case["expected"])
+
+    def test_block_to_block_type(self):
+        test_cases = [
+            # start with "###"
+            {
+                "input": "### This is a heading",
+                "expected": Block_Type.HEADING,
+                "exception": None
+            },
+            # start with "```"
+            {
+                "input": "``` This is a code block\n```\nThis is a code block\n```",
+                "expected": Block_Type.CODE,
+                "exception": None
+            },
+            # every line starts with ">"
+            {
+                "input": "> This is a quote\n> This is another quote\n> This is the last quote",
+                "expected": Block_Type.QUOTE,
+                "exception": None
+            },
+            # every line starts with "* "
+            {
+                "input": "* This is a list item\n* This is another list item\n* This is the last list item",
+                "expected": Block_Type.UNORDERED_LIST,
+                "exception": None
+            },
+            # every line starts with "- "
+            {
+                "input": "- This is a list item\n- This is another list item\n- This is the last list item",
+                "expected": Block_Type.UNORDERED_LIST,
+                "exception": None
+            },
+            # every line starts with "x. " x += 1
+            {
+                "input": "1. This is a list item\n2. This is another list item\n3. This is the last list item",
+                "expected": Block_Type.ORDERED_LIST,
+                "exception": None
+            },
+            # normal single line
+            {
+                "input": "This is a paragraph",
+                "expected": Block_Type.PARAGRAPH,
+                "exception": None
+            },
+            # normal multi line
+            {
+                "input": "This is a paragraph\nThis is another paragraph",
+                "expected": Block_Type.PARAGRAPH,
+                "exception": None
+            },
+            # edge case
+            {
+                "input": "...This is a paragraph...This is another paragraph",
+                "expected": Block_Type.PARAGRAPH,
+                "exception": None
+            },
+        ]
+
+        for case in test_cases:
+            with self.subTest(case=case):
+                if case["exception"]:
+                    with self.assertRaises(case["exception"]):
+                        block_to_block_type(case["input"])
+                else:
+                    actual = block_to_block_type(case["input"])
                     self.assertEqual(actual, case["expected"])
 
 if __name__ == "__main__":
